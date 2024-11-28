1c28ab46f8960c0bfa982a6d25dd141c60b3af00
skx1322 <kaixing636@gmail.com>
2024-11-28 17:27:21 +0800

    Fixed Google auth issue in the auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -140,7 +140,7 @@ env.config();
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "http://localhost:3000/auth/google/secrets",
+                callbackURL: "http://blog-web-djid.onrender.com/auth/google/secrets",
                 userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
             },
             async(accessToken, refreshToken, profile, cb)=>{
